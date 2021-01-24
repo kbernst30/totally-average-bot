635f9ad4bb6eac2ee9ce1e8c79d2771f33fde541
kbernst30 <kbernst30@gmail.com>
2021-01-24 11:41:27 -0500

    Added podcast command

--- commands.py
+++ commands.py
@@ -11,6 +11,11 @@ YOUTUBE_LATEST_REQUEST_URI = (
     "https://www.googleapis.com/youtube/v3/search?key=%s&part=snippet&channelId=%s&order=date&type=video"
 )
 
+EMBED_COLOR = 0x36457A
+PODCAST_URL = "https://anchor.fm/totally-average-gamers"
+PODCAST_IMG_URL = "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/" + \
+    "podcast_uploaded_nologo400/11978152/11978152-1610747060322-634d739912be4.jpg"
+
 
 def register_commands(bot):
     load_dotenv()
@@ -31,10 +36,27 @@ def register_commands(bot):
         if len(latest_youtube_videos) == 0:
             embed = discord.Embed(
                 description="I can't seem to find any recent videos on YouTube... how average :neutral_face:",
-                color=0x36457A
+                color=EMBED_COLOR
             )
 
             await ctx.send(embed=embed)
 
         else:
             await ctx.send("Need a video here")
+
+    @bot.command(name="podcast", help="Gets the public URL of the Totally Average Gamers podcast")
+    async def send_podcast(ctx):
+        logger.info(bot.user.avatar_url)
+        embed = discord.Embed(
+            title="The Totally Average Gamers Podcast",
+            url=PODCAST_URL,
+            color=EMBED_COLOR
+        )
+
+        embed.set_image(url=PODCAST_IMG_URL)
+        embed.set_author(name="Totally Average Gamers", icon_url=bot.user.avatar_url)
+
+        await ctx.send(
+            "Check out the latest episodes of the Totally Average Gamers podcast!",
+            embed=embed
+        )
